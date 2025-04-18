72db5b4e878165ebf2a3417ef84b75ff4b4baef8
zahid4kh <halilzahid@gmail.com>
2025-04-18 22:59:23 +0200

    closing database when app is exited

--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -7,6 +7,8 @@ import androidx.compose.ui.window.rememberWindowState
 import java.awt.Dimension
 import zahid4kh.qrchitect.App
 import org.jetbrains.compose.reload.DevelopmentEntryPoint
+import org.koin.java.KoinJavaComponent.getKoin
+import zahid4kh.qrchitect.data.QrDatabase
 import javax.swing.UIManager
 
 fun main() {
@@ -21,7 +23,17 @@ fun main() {
         Window(
             title = "QRchitect",
             state = rememberWindowState(width = 1000.dp, height = 700.dp),
-            onCloseRequest = ::exitApplication,
+            onCloseRequest = {
+                try {
+                    val database = getKoin().get<QrDatabase>()
+                    database.close()
+                    println("Database connection closed successfully")
+                } catch (e: Exception) {
+                    println("Error closing database: ${e.message}")
+                    e.printStackTrace()
+                }
+                exitApplication()
+            },
         ) {
             window.minimumSize = Dimension(800, 600)
             DevelopmentEntryPoint {
